37b6976bdc27579207da644fecc4d0d002e8db28
ArtemkAmg <atamancuck2006@gmail.com>
2026-09-05 11:53:12 +0300

    Initial commit

--- obj/Debug/net8.0/MovieHubMvc.AssemblyInfo.cs
+++ obj/Debug/net8.0/MovieHubMvc.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MovieHubMvc")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c7b7c9ed6ea538ed67a0db7f32055950824cbfb5")]
 [assembly: System.Reflection.AssemblyProductAttribute("MovieHubMvc")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MovieHubMvc")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
